e283951ee619abf413235a357589aaf2e37b7367
muratsalakhov <muratsalakhov@gmail.com>
2023-10-20 23:54:54 +0300

    feat: Связи в моделях

--- app/Models/HabitCheck.php
+++ app/Models/HabitCheck.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class HabitCheck extends Model
 {
     use HasFactory;
+
+    public function habit(): BelongsTo
+    {
+        return $this->belongsTo(Habit::class);
+    }
 }
--- app/Models/TimetableSlot.php
+++ app/Models/TimetableSlot.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class TimetableSlot extends Model
 {
     use HasFactory;
+
+    public function timetable(): BelongsTo
+    {
+        return $this->belongsTo(Timetable::class);
+    }
 }

0af1cf867f94e2219d89fd70a2e8c31ef9268540
muratsalakhov <muratsalakhov@gmail.com>
2024-03-05 22:17:09 +0300

    feat: Сервис регистрации

--- app/Services/Auth/RegistrationService.php
+++ app/Services/Auth/RegistrationService.php
@@ -2,7 +2,26 @@
 
 namespace App\Services\Auth;
 
+use App\DTO\Auth\UserRegisterDTO;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+/**
+ * Сервис регистрации
+ */
 class RegistrationService
 {
-    // todo
+    /**
+     * Зарегистрировать пользователя
+     * @param UserRegisterDTO $userRegisterDTO
+     * @return User
+     */
+    public function register(UserRegisterDTO $userRegisterDTO): User
+    {
+        return User::create([
+            'name' => $userRegisterDTO->name,
+            'email' => $userRegisterDTO->email,
+            'password' => Hash::make($userRegisterDTO->password)
+        ]);
+    }
 }

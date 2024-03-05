ad3a0669f1d1b479d6e82f2cdca6a39fa1b17a2b
muratsalakhov <muratsalakhov@gmail.com>
2024-03-05 22:17:01 +0300

    feat: Сервис авторизации

--- app/Services/Auth/AuthService.php
+++ app/Services/Auth/AuthService.php
@@ -2,7 +2,52 @@
 
 namespace App\Services\Auth;
 
+use App\Models\User;
+use Auth;
+use Laravel\Sanctum\NewAccessToken;
+
+/**
+ * Сервис авторизации
+ */
 class AuthService
 {
-    // todo
+    /**
+     * Авторизовать
+     * @param string $email
+     * @param string $password
+     * @return User|null
+     */
+    public function login(string $email, string $password): ?User
+    {
+        $credentials = [
+            'email' => $email,
+            'password' => $password
+        ];
+
+        if (Auth::attempt($credentials)) {
+            return Auth::user();
+        }
+
+        return null;
+    }
+
+    /**
+     * Разавторизовать
+     * @param User $user
+     * @return void
+     */
+    public function logout(User $user): void
+    {
+        $user->currentAccessToken()->delete();
+    }
+
+    /**
+     * Создать токен для пользователя
+     * @param User $user
+     * @return NewAccessToken
+     */
+    public function createToken(User $user): NewAccessToken
+    {
+        return $user->createToken('auth');
+    }
 }

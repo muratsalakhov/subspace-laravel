b008433f97cde094d9227eba582212301dc2eac5
muratsalakhov <muratsalakhov@gmail.com>
2024-03-07 00:16:20 +0300

    feat: Данные авторизованного пользователя

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,13 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Http\Helpers\ApiResponse;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Auth;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
+use Knuckles\Scribe\Attributes\Endpoint;
+use Knuckles\Scribe\Attributes\Group;
+use Knuckles\Scribe\Attributes\ResponseFromApiResource;
 
 class UserController extends Controller
 {
-    public function show(Request $request): JsonResponse
+    #[
+        Group('Пользователь'),
+        Endpoint('Данные пользователя', 'Получить данные авторизованного пользователя'),
+        ResponseFromApiResource(UserResource::class, User::class)
+    ]
+    public function show(): JsonResponse
     {
-        return ApiResponse::success([]);
+        return ApiResponse::success(
+            UserResource::make(Auth::user())
+        );
     }
 }

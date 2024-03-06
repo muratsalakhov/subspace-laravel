663103c9b2c4a0483fe7cc426248174992e48cdb
muratsalakhov <muratsalakhov@gmail.com>
2024-03-06 23:52:57 +0300

    feat: Имя в ресурсе пользователя

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,7 @@ class UserResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'name' => $this->name,
             'email' => $this->email
         ];
     }

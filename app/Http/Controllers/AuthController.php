f09901729dc1195497313708432a13008737a3a6
muratsalakhov <muratsalakhov@gmail.com>
2024-03-06 23:53:14 +0300

    docs: Документация scribe для контроллера авторизации

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,16 +11,26 @@ use App\Services\Auth\RegistrationService;
 use Auth;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Knuckles\Scribe\Attributes\BodyParam;
+use Knuckles\Scribe\Attributes\Endpoint;
+use Knuckles\Scribe\Attributes\Group;
+use Knuckles\Scribe\Attributes\Response;
 
 class AuthController extends Controller
 {
-
     public function __construct(
         private readonly AuthService $authService,
         private readonly RegistrationService $registrationService
     ) {
     }
 
+    #[
+        Group('Регистрация/авторизация'),
+        Endpoint('Авторизация', 'Авторизовать пользователя'),
+        BodyParam('email', description: 'Email, указанный при регистрации', example: 'newuser@example.com'),
+        BodyParam('password', description: 'Пароль, указанный при регистрации', example: 'newsecret'),
+        Response(content: '{"token": "token", "user": {"id": 1, "name": "Иван Новый", "email": "newuser@example.com"}}', status: 200, description: "Успешная авторизация")
+    ]
     public function login(Request $request): JsonResponse
     {
         $validatedData = $request->validate([
@@ -37,6 +47,11 @@ class AuthController extends Controller
         return $this->getAuthResponse($user);
     }
 
+    #[
+        Group('Регистрация/авторизация'),
+        Endpoint('Выход из системы', 'Разавторизовать пользователя'),
+        Response(content: '{"message": "Пользователь вышел из системы"}', status: 200, description: 'Пользователь вышел из системы')
+    ]
     public function logout(): JsonResponse
     {
         $user = Auth::user();
@@ -53,12 +68,20 @@ class AuthController extends Controller
     /**
      * @throws UserAlreadyExistsException
      */
+    #[
+        Group('Регистрация/авторизация'),
+        Endpoint('Регистрация', 'Зарегистрировать пользователя'),
+        BodyParam('name', description: 'Максимальная длина 255 символов', example: 'Иван Новый'),
+        BodyParam('email', description: 'Должен быть действительным электронным адресом, максимальная длина 255 символов, уникальный в таблице users', example: 'newuser@example.com'),
+        BodyParam('password', description: 'Минимум 6 символов', example: 'newsecret'),
+        Response(content: '{"token": "token", "user": {"id": 1, "name": "Иван Новый", "email": "newuser@example.com"}}', status: 200, description: "Успешная регистрация")
+    ]
     public function register(Request $request): JsonResponse
     {
         $validatedData = $request->validate([
-            'name' => 'required',
-            'email' => 'required|email|unique:users,email',
-            'password' => 'required',
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:users,email',
+            'password' => 'required|string|min:6',
         ]);
 
         $user = $this->registrationService->register(

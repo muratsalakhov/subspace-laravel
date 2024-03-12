53ebb782548f0a47764d2a568316e5756fd374d2
muratsalakhov <muratsalakhov@gmail.com>
2024-03-13 02:40:19 +0300

    feat: Отключение неиспользуемого обработчика

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -17,7 +17,6 @@ class EventServiceProvider extends ServiceProvider
      */
     protected $listen = [
         Registered::class => [
-            SendEmailVerificationNotification::class,
             CreateDefaultUserData::class
         ],
     ];

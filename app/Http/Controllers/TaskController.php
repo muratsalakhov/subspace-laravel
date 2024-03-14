ee79f7d4c347e30557aab26f2398d48aa6e2524f
muratsalakhov <muratsalakhov@gmail.com>
2024-03-14 20:11:35 +0300

    feat: Методы контроллера задачи

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,50 +3,90 @@
 namespace App\Http\Controllers;
 
 use App\Http\Helpers\ApiResponse;
+use App\Http\Resources\TaskResource;
 use App\Models\Task;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class TaskController extends Controller
 {
+    use RequiresAuthenticatedUser;
+
     public function index(): JsonResponse
     {
-        $this->authorize('viewAny');
+        $this->authorize('viewAny', Task::class);
 
-        return ApiResponse::success([]);
+        $user = $this->getUser();
+
+        return ApiResponse::success(
+            TaskResource::collection($user->tasks)
+        );
     }
 
     public function show(Task $task): JsonResponse
     {
         $this->authorize('view', $task);
 
-        return ApiResponse::success([]);
+        return ApiResponse::success(
+            TaskResource::make($task)
+        );
     }
 
-    public function store(): JsonResponse
+    public function store(Request $request): JsonResponse
     {
-        $this->authorize('create');
+        $this->authorize('create', Task::class);
 
-        return ApiResponse::success([]);
+        $validatedData = $this->validate($request, [
+            'name' => 'required|string|max:255',
+        ]);
+
+        $user = $this->getUser();
+
+        $task = $user->tasks()->create($validatedData);
+
+        return ApiResponse::success(
+            TaskResource::make($task)
+        );
     }
 
-    public function update(Task $task): JsonResponse
+    public function update(Request $request, Task $task): JsonResponse
     {
         $this->authorize('update', $task);
 
-        return ApiResponse::success([]);
+        $validatedData = $this->validate($request, [
+            'name' => 'sometimes|string|max:255',
+            'is_completed' => 'sometimes|boolean',
+        ]);
+
+        $task->update($validatedData);
+
+        return ApiResponse::success(
+            TaskResource::make($task)
+        );
     }
 
-    public function updateStatus(Task $task): JsonResponse
+    public function updateStatus(Request $request, Task $task): JsonResponse
     {
         $this->authorize('updateStatus', $task);
 
-        return ApiResponse::success([]);
+        $validatedData = $this->validate($request, [
+            'is_completed' => 'required|boolean',
+        ]);
+
+        $task->update($validatedData);
+
+        return ApiResponse::success(
+            TaskResource::make($task)
+        );
     }
 
     public function destroy(Task $task): JsonResponse
     {
         $this->authorize('delete', $task);
 
-        return ApiResponse::success([]);
+        $task->delete();
+
+        return ApiResponse::success([], code: Response::HTTP_NO_CONTENT);
     }
 }
